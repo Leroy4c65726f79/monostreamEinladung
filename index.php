4200a18db7524c3727e1ea3188d343242f1de1f3
Leroy <4c65726f79@gmail.com>
2018-06-25 23:27:13 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- .................................sheets.................................-->
+    
     <link rel="stylesheet" href="style/normalize.css">
     <link rel="stylesheet" href="style/style.min.css">
     <script src="js/jquery-3.3.1.min.js"></script>
